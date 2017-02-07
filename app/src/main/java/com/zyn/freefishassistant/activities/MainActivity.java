da2e556c5685904e84392203091e4f6fe145fa7f
ZengYinan <498338021@qq.com>
2017-02-07 21:22:44 +0800

    initial commit

---
+++ app/src/main/java/com/zyn/freefishassistant/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zyn.freefishassistant.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zyn.freefishassistant.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
